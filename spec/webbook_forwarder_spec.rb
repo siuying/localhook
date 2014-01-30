8269449b5f0b2b984072c27a733c523b51361dbd
Francis Chong <francis@ignition.hk>
2014-01-30 11:59:39 +0800

    add test for forwarder post

--- spec/webbook_forwarder_spec.rb
+++ spec/webbook_forwarder_spec.rb
@@ -23,4 +23,16 @@ describe Localhook::WebhookForwarder do
       }.to raise_error(ArgumentError)
     end
   end
+
+  context "-post" do
+    subject { Localhook::WebhookForwarder.new("http://localhost:8080") }
+    it "forward request to remote server via em" do
+      params = {head: {}, path: "/a/b", query: "", body: ""}
+
+      mock_em = stub(:mock_em)
+      expect(mock_em).to receive(:post).with(params)
+      expect(EventMachine::HttpRequest).to receive(:new).with(subject.url, subject.http_options).and_return(mock_em)
+      subject.post("/a/b", "", {}, "")
+    end
+  end
 end
